8fa0dd2702fdffd40d8afaa071b34f094331cb87
Bullishdrake09 <97606346+Bullishdrake09@users.noreply.github.com>
2024-03-13 18:27:40 +0100

    Update Plugin.cs

--- HitScoreVisualizer/Plugin.cs
+++ HitScoreVisualizer/Plugin.cs
@@ -10,22 +10,25 @@ using SiraUtil.Zenject;
 
 namespace HitScoreVisualizer
 {
-	[Plugin(RuntimeOptions.DynamicInit), NoEnableDisable]
-	public class Plugin
-	{
-		internal static Version Version { get; private set; } = null!;
+    [Plugin(RuntimeOptions.DynamicInit)]
+    public class Plugin
+    {
+        internal static Version Version { get; private set; } = null!;
 
-		[Init]
-		public void Init(Logger logger, Config config, PluginMetadata pluginMetadata, Zenjector zenject)
-		{
-			Version = pluginMetadata.HVersion;
+        [Init]
+        public void Init(Logger logger, Config config, PluginMetadata pluginMetadata, Zenjector zenject)
+        {
+            Version = pluginMetadata.Version;
 
-			zenject.UseLogger(logger);
-			zenject.UseMetadataBinder<Plugin>();
+            zenject.UseLogger(logger);
+            zenject.UsePluginId("com.hitScoreVisualizer.plugin");
 
-			zenject.Install<HsvAppInstaller>(Location.App, config.Generated<HSVConfig>());
-			zenject.Install<HsvMenuInstaller>(Location.Menu);
-			zenject.Install<HsvGameInstaller>(Location.Tutorial | Location.Player);
-		}
-	}
+            zenject.OnApp<HsvAppInstaller>().WithConfig<HsvAppInstaller, HSVConfig>(config);
+            zenject.OnMenu<HsvMenuInstaller>().OnlyForMultiplayer();
+            zenject.OnGame<HsvGameInstaller>().OnlyForStandard();
+
+            // If you want to install for specific locations like Tutorial and Player, you might need to handle it differently.
+            // It's suggested to use the provided hooks like OnApp, OnMenu, OnGame for better compatibility across different Beat Saber versions.
+        }
+    }
 }
