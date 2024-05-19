e7a2b91d5f230159aea310cae9efe5b6eb5330d2
Bullishdrake09 <97606346+Bullishdrake09@users.noreply.github.com>
2024-05-19 17:19:34 +0200

    Delete HitScoreVisualizer directory

--- HitScoreVisualizer/Helpers/Json/ShouldNotSerializeAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace HitScoreVisualizer.Helpers.Json
-{
-	[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
-	internal sealed class ShouldNotSerializeAttribute : Attribute
-	{
-	}
-}
